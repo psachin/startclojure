45a37f9d6587310494fbba936e1abfb9e240e690
psachin <iclcoolster@gmail.com>
2014-07-28 12:02:10 +0530

    # commit.template # This file is use to set default message when we commit
    Subject: URL shorten
    
    Description:
    
    - Compile to file: app.clj using C-c C-k
    - execute the server line(it is commented)
    
    - Add some urls, for example:
      (shorten "https://github.com/psachin")
    
    - Display the map: 'urls' using 'urls' in nrepl
    
    - Visit the browser localhost:8080/<ID>
    
    - It should redirect to actual URL.

--- src/startclojure/app.clj
+++ src/startclojure/app.clj
@@ -1,13 +1,37 @@
 (ns startclojure.app
-  (:require [ring.adapter.jetty :as jetty])
-  )
+  ;; (:use (clojure.pprint))
+  (:use (compojure handler
+                   [core :only (GET POST defroutes)]))
+  (:require [ring.util.response :as response]
+            [ring.adapter.jetty :as jetty]))
 
-(use 'clojure.pprint)
+;; (use 'clojure.pprint)
 
-(defn app
+(defonce counter (atom 1000))
+
+(defonce urls (atom {}))
+
+(defn shorten
+  [url]
+  (let [id (swap! counter inc)
+        id (Long/toString id 36)]
+    (swap! urls assoc id url)
+    id))
+
+
+(defn homepage
   [request]
-  {:status 200
-   :body (with-out-str
-           (pprint request))})
+  (str @urls))
+
+
+(defn redirect
+  [id]
+  (response/redirect (@urls id)))
+
+
+(defroutes app
+  (GET "/" request (homepage request))
+  (GET "/:id" [id] (redirect id)))
+
 
 ;; (def server (jetty/run-jetty #'app {:port 8080 :join? false}))
